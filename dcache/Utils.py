c8e2e7499db4c7aa05bb2174e6fdfbb792cb5e27
Mathilde Koch <mathilde.koch@inra.fr>
2019-05-27 16:56:44 +0200

    Include stoechiometry in DB

--- dcache/Utils.py
+++ dcache/Utils.py
@@ -23,12 +23,13 @@ def make_document_id(rule_id, substrate_id):
     return rule_id + "_" + substrate_id
 
 
-def as_document(rule_id, substrate_id, list_list_inchikeys=[], list_list_inchis=[], list_list_smiles=[]):
+def as_document(rule_id, substrate_id, list_list_inchikeys=[], list_list_inchis=[], list_list_smiles=[],
+                list_stoechiometry = []):
     """
     Format data to be inserted into the cache DB.
-    
+
     Targeted DB is Mongo.
-    
+
     :param  rule_id:                unique ID for the rule
     :param  substrate_id:           unique ID for the substrate
     :param  list_list_inchikeys:    list of list of InchiKeys
@@ -42,14 +43,15 @@ def as_document(rule_id, substrate_id, list_list_inchikeys=[], list_list_inchis=
         "substrate_id": substrate_id,
         "list_list_inchikeys": list_list_inchikeys,
         "list_list_inchis": list_list_inchis,
-        "list_list_smiles": list_list_smiles
+        "list_list_smiles": list_list_smiles,
+        "list_stoechiometry": list_stoechiometry
     }
 
 
 def rdmols_from_document(document, build_from="inchi", add_hs=True):
     """
     Convert back a document to a set of rdmols. This method is a companion of "as_document".
-    
+
     :param document: a document produced by the "as_mongo_document" method, dict
     :param build_from: the type of depiction to be used to build back the rdmols, str in ["inchi", "smiles"]
     :param add_hs: add Hs to RDKit mol object, default is True
@@ -59,7 +61,7 @@ def rdmols_from_document(document, build_from="inchi", add_hs=True):
     assert add_hs in [True, False]
 
     list_list_rdmols = list()
-
+    list_stoechiometry = document['list_stoechiometry']
     if build_from == 'inchi':
         for list_inchis in document['list_list_inchis']:
             list_rdmols = list()
@@ -81,4 +83,4 @@ def rdmols_from_document(document, build_from="inchi", add_hs=True):
     else:
         raise NotImplementedError()
 
-    return list_list_rdmols
+    return list_list_rdmols, list_stoechiometry
