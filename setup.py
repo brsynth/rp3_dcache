af53551977fceecf6831e9130ee06b2af2dced07
Thomas Duigou <thomas.duigou@inra.fr>
2019-09-16 15:21:14 +0200

    Package name update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-        name="dcache",
-        version="0.1",
+        name="rp3_dcache",
+        version="0.2",
         author="Thomas Duigou",
         author_email="thomas.duigou@inra.fr",
-        description="Cache to dope the result retrieval from reaction rules",
+        description="Cache to dope the result retrieval from reaction rules, to be used with RetroPath 3.0",
         packages=setuptools.find_packages()
 )
