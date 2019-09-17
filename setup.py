0e78aee8de158ef767ca1266ed1c727501484b9d
Mathilde Koch <mathilde.koch@inra.fr>
2019-09-17 15:44:45 +0200

    include manifest for non-coding files

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
         name="rp3_dcache",
-        version="0.2",
+        version="0.3",
         author="Thomas Duigou",
         author_email="thomas.duigou@inra.fr",
         description="Cache to dope the result retrieval from reaction rules, to be used with RetroPath 3.0",
-        packages=setuptools.find_packages()
+	long_description=long_description,
+    	long_description_content_type="text/markdown",
+	url="https://github.com/brsynth/rp3_dcache",
+        packages=setuptools.find_packages(),
+	python_requires='>=3.6',
+	include_package_data=True
 )
